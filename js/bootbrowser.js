a69a4d6e1f7295a22c010de18c5f084dca6722df
Dan Borthwick <danborthwick@hotmail.com>
2017-10-08 12:24:15 +1100

    Dynamic search page

--- js/bootbrowser.js
+++ js/bootbrowser.js
@@ -22,12 +22,15 @@ myApp.onPageInit('search', function (page) {
 });
 
 var html = compiledSearchTemplate({
-		selected: { id: 1 },
+		selected: { id: 1, image: 'images/Shoe/http-_2F_2Fstatic.theiconic.com.au_2Fp_2Flipstik-9248-621344-1.jpg' },
+
 		suggestions: [
-			{ id: 2},
-			{ id: 3},
-			{ id: 4},
-			{ id: 5},
+			{ newRow: true,
+				id: 2, image: 'images/shoe%202/http-_2F_2Fstatic.theiconic.com.au_2Fp_2Firo-9282-952115-1.jpg' },
+			{ id: 3, image: 'images/shoe%203/http-_2F_2Fstatic.theiconic.com.au_2Fp_2Fskin-0979-311184-1.jpg' },
+			{ newRow: true,
+				id: 4, image: 'images/shoe%204/http-_2F_2Fstatic.theiconic.com.au_2Fp_2Fcoconuts-by-matisse-1058-546784-1.jpg' },
+			{ id: 5, image: 'images/shoe%205/http-_2F_2Fstatic.theiconic.com.au_2Fp_2Fsol-sana-9277-333544-1.jpg' },
 		]
 	});
 mainView.router.loadContent(html);
